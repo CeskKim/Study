bf0b9b76bc0a799aa44bcf42a4701cb01de82ab2
CeskKim <pkuuu@naver.com>
2020-10-01 21:24:43 +0900

    Update Running Query Monitor Scripts.sql

--- MSSQL/Scripts/Running Query Monitor Scripts.sql
+++ MSSQL/Scripts/Running Query Monitor Scripts.sql
@@ -1,13 +1,13 @@
 USE ExampleDB
 GO
 /*************************************************************************************************
- Ω««‡¡ﬂ ƒı∏Æ ¡∂»∏
+ Ïã§ÌñâÏ§ë ÏøºÎ¶¨ Ï°∞Ìöå
 **************************************************************************************************/
 
 SELECT  [TEXT],
 		SUBSTRING(st.[TEXT], (qs.statement_start_offset/2)+1, 
 		((CASE qs.statement_end_offset	WHEN -1 
-										THEN DATALENGTH(st.[TEXT])
+						THEN DATALENGTH(st.[TEXT])
 		  ELSE qs.statement_end_offset	END - qs.statement_start_offset)/2) + 1) 
 		AS statement_text,* 
   FROM  sys.dm_exec_requests AS qs
@@ -15,7 +15,7 @@ SELECT  [TEXT],
   CROSS APPLY sys.dm_exec_query_plan(plan_handle)
 
  /*************************************************************************************************
- ¬¸¡∂ªÁ¿Ã∆Æ
+ Ï∞∏Ï°∞ÏÇ¨Ïù¥Ìä∏
  https://ittutorial.org/sql-server-dba-scripts-all-in-one-useful-ms-sql-dba-database-administration-scripts/?fbclid=IwAR0sronvPn0lshQ9nYNqaLKP6FnOZ_9I3oq1T1kkGMQnYNQDkQ7Xzxag6JE
 **************************************************************************************************/
 
