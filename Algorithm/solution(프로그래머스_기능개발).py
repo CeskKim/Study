49bc0122715474bb4cabfbbc228970ec98f41e43
CeskKim <pkuuu@naver.com>
2021-09-12 23:02:12 +0900

    Update solution(프로그래머스_기능개발).py

--- Algorithm/solution(프로그래머스_기능개발).py
+++ Algorithm/solution(프로그래머스_기능개발).py
@@ -2,10 +2,9 @@ def solution(progresses, speeds):
     answer = []
     
     for p, s in zip(progresses, speeds):
-        if len(answer) == 0 or answer[-1][0] < - ((p - 100) / s):
-            answer.append([-((p - 100) / s), 1])
+        if len(answer) == 0 or answer[-1][0] < - ((p - 100) // s):
+            answer.append([-((p - 100) // s), 1])
         else:
             answer[-1][1] += 1
  
     return [reanswer[1] for reanswer in answer]
- 
